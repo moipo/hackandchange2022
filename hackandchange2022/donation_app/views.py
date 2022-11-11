dcf666727885348108d903f0f501affe9fca259a
moipo <moipochtoviyyashik@inbox.ru>
2022-11-11 22:27:32 +0300

    basic django structure

---
+++ hackandchange2022/donation_app/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+
+
+
+class General:
+    def base(request):
+        ctx = {
+        "data" : "DATA FROM THE SERVER"
+        }
+        return render(request,"donation_app/base.html" , ctx)
