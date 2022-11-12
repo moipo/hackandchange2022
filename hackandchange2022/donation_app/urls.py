bce9daf21a65a6553779b776454c9e888103cc31
moipo <moipochtoviyyashik@inbox.ru>
2022-11-12 11:09:56 +0300

    half-finished js charts

--- hackandchange2022/donation_app/urls.py
+++ hackandchange2022/donation_app/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', General.base , name = "base")
+    path('', General.base , name = "base"),
+    path('streamer/analytics', General.streamer_analytics, name = "streamer_analytics")
 ]
--- hackandchange2022/donation_app/views.py
+++ hackandchange2022/donation_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from .models import *
 
 
 
@@ -9,3 +9,19 @@ class General:
         "data" : "DATA FROM THE SERVER"
         }
         return render(request,"donation_app/base.html" , ctx)
+
+    def streamer_analytics(request):
+        # user = request.user
+        # streamer =  Streamer.objects.filter(user = user)[0]
+        # st_donations = streamer.donation_set.all()
+        # ctx = {
+        # "st_donations": st_donations,
+        # }
+        ctx = {}
+        return render(request, "donation_app/streamer_analytics.html", ctx)
+
+
+
+
+# class Donation:
+#     def create_donation(request):
