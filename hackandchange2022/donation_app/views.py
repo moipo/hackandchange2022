2a86843455ba8424749d74ff494dc09310b50ed9
moipo <moipochtoviyyashik@inbox.ru>
2022-11-12 14:06:58 +0300

    add table with all the streamer donations

--- hackandchange2022/donation_app/views.py
+++ hackandchange2022/donation_app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .models import *
-
+from functools import reduce
 
 
 class General:
@@ -14,21 +14,38 @@ class General:
     def streamer_analytics(request):
         user = request.user
         streamer =  Streamer.objects.filter(user = user)[0]
-        st_donations = streamer.donation_set.all()
+        all_st_donations = streamer.donation_set.all()
+
+
 
         data_last = []
         labels_last = []
-        for donation in st_donations:
+        for donation in all_st_donations:
             data_last.append(donation.price)
             labels_last.append(donation.date_created.strftime(" %d.%m %H:%M"))
 
+
+
         # data_last = [6,3,1,8,6,3]
         # labels_last = ["day1", "day2","day3","day4","day5","day6"]
+
+
+        #chart2(sum)
+        data_sum = []
+        for i in range(len(data_last)):
+            if i > 0: data_sum.append(data_last[i] + data_last[i-1])
+            else: data_sum.append(data_last[i] + 0)
+
+
+
+
+
         ctx = {
         "data_last":data_last,
         "labels_last":labels_last,
-        "data_sum":data_last,
+        "data_sum":data_sum,
         "labels_sum":labels_last,
+        "all_st_donations": all_st_donations
         }
         return render(request, "donation_app/streamer_analytics.html", ctx)
 
